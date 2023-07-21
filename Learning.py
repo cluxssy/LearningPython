2e3f3029fdc7f1cd917bb397d2ceee38067f959c
Priyanshu Sharma <cluxssy25@gmail.com>
2023-07-21 15:42:50 +0530

    Add files via upload

---
+++ Learning.py
@@ -0,0 +1,13 @@
+print("Priaynshu")
+print("0" * 10)
+monkey = 24
+print (monkey)
+monkey = 36
+print (monkey)
+is_male = True
+name = "John_Smith"
+age = 20
+is_new = True
+name = input("What is your name? ")
+favourite_colour = input("What is your favourite colour? ")
+print(name + ' likes'+ favourite_colour)
