005a89a6b5841d06921fe3c35e67e03477e33195
Priyanshu Sharma <cluxssy25@gmail.com>
2023-07-28 00:38:17 +0530

    Add files via upload

--- Main.py
+++ Main.py
@@ -1,7 +1,6 @@
 print(1)
 print("hello world")
 name = "Priyanshu"
-age = 18
 print(name)
 last_name = "stark"
 first_name = "Tony"
---
+++ While Loops.py
@@ -0,0 +1,7 @@
+i = 1
+while i <= 176:
+    print("*" * i)
+    i = i + 1
+
+
+print('Done')
